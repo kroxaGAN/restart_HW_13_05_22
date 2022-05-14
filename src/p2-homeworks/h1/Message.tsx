e32ae1937f6f36e72639394c2454dba2af5af7ba
Andrei Hutsau <mwplt@mail.ru>
2022-05-14 09:13:50 +0300

    style not perfect, but my

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -12,13 +12,11 @@ export const Message = ({avatar, name, message, time}: PropsType) => {
     return (
         <div className={s.message}>
             <img src={avatar} alt={''} className={s.img}/>
-            <div className={s.circle}>''</div>
+            <div className={s.circle}/>
             <div className={s.textMes}>
-                {name}
+                <h3>{name}</h3>
                 <div className={s.textTime}>
-                    <div className={s.name}>
-                        {message}
-                    </div>
+                    <div className={s.text}>{message}</div>
                     <div className={s.time}>{time}</div>
                 </div>
             </div>
